87d9dbf8402b2870d7690008518cdc827eb3e5a4
Jeff Muthunga <jeff.wainaina@student.moringaschool.com>
2023-03-14 05:18:41 +0300

    Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create([{first_name: "Jeff", last_name: "Wainaina", grade: 12}, 
+                {first_name:"Albar", last_name:"Mutugu", grade: 12}])
